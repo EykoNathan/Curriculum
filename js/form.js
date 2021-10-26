0fc070fbe58590ad68acb49248659f37b11dc123
EykoNathan <90914758+EykoNathan@users.noreply.github.com>
2021-10-26 11:53:08 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -11,3 +11,51 @@ function Mudarestado() {
             pegaBotao.innerHTML = "Ocultar detalhes adicionais";
         }
     }
+function mostraDivEmpregos() {
+    var divEmpregos = document.getElementById('divEmpregos');
+    var conteudo = divEmpregos.innerHTML + conteudoDivEmpregos();
+    divEmpregos.innerHTML =  conteudo;
+}
+function conteudoDivEmpregos() { 
+    return `
+    <div class="container-emprego">
+        <div class="row linha1">
+            <div class="col-md-6 cargo2">
+                <label for="cargo"><strong>Cargo:</strong></label>
+                <input type="text" name="cargo" id="cargo2" class="form-control" tabindex="1">
+            </div>
+            <div class="col-md-6 emprego2">
+                <label for="empregador"><strong>Empregador:</strong></label>
+                <input class="form-control" type="text" name="empregador">
+            </div>
+        </div>
+        <div class="row linha2">
+            <div class="col-md-6 data1">
+                <label for="data"><strong>Data de inicio:</strong></label>
+                <input type="date" name="data" id="data" class="form-control" tabindex="1">
+            </div>
+            <div class="col-md-6 data2">
+                <label for="data"><strong>Data de termino:</strong></label>
+                <input type="date" name="data2" id="data2" class="form-control" tabindex="1">
+            </div>
+        </div>
+        <div class="row linha3">
+            <div class="col-md-6 estado2">
+                <label for="estado"><strong>Estado:</strong></label>
+                <input type="text" name="estado" id="estado" class="form-control" tabindex="1">
+            </div>
+            <div class="col-md-6 cidade2">
+                <label for="empregador"><strong>Cidade:</strong></label>
+                <input type="text" name="cidade" id="cidade" class="form-control" tabindex="1">
+            </div>
+        </div>
+        <div id="dvCentro">
+            <textarea id="txtArtigo" name="txtArtigo"></textarea>
+        </div>
+        script src="ckeditor/ckeditor/ckeditor.js"></script>
+        <script>
+            CKEDITOR.replace('txtArtigo');
+        </script>
+    </div>
+    `; 
+}
