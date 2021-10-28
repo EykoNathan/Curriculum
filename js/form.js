b2b5c508f33134e9d659d85a34edbcafe8cba0d1
EykoNathan <90914758+EykoNathan@users.noreply.github.com>
2021-10-27 22:13:19 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -49,13 +49,82 @@ function conteudoDivEmpregos() {
                 <input type="text" name="cidade" id="cidade" class="form-control" tabindex="1">
             </div>
         </div>
-        <div id="dvCentro">
-            <textarea id="txtArtigo" name="txtArtigo"></textarea>
-        </div>
-        script src="ckeditor/ckeditor/ckeditor.js"></script>
-        <script>
-            CKEDITOR.replace('txtArtigo');
-        </script>
     </div>
+    <div id="dvCentro">
+        <textarea id="txtArtigo" name="txtArtigo"></textarea>
+    </div>
+    script src="ckeditor/ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace('txtArtigo');
+    </script>
+    `; 
+}
+function mostraDivLinks() {
+    var divLinks = document.getElementById('divLinks');
+    var conteudo = divLinks.innerHTML + conteudoDivLinks();
+    divLinks.innerHTML =  conteudo;
+}
+function conteudoDivLinks() { 
+    return `
+    <div class="container-link">
+        <div class="row linha1">
+            <div class="col-md-6 rotulo">
+                <label for="rotulo"><strong>Rotulo:</strong></label>
+                <input type="text" name="rotulo" id="rotulo" class="form-control" tabindex="1">
+            </div>
+            <div class="col-md-6 link">
+                <label for="link"><strong>Link:</strong></label>
+                <input class="form-control" type="text" name="link">
+            </div>
+        </div>
+    </div>
+    `; 
+}
+function mostraDivEducação() {
+    var divEducação = document.getElementById('divEducacao');
+    var conteudo = divEducação.innerHTML + conteudoDivEducação();
+    divEducação.innerHTML =  conteudo;
+}
+function conteudoDivEducação() { 
+    return `
+    <div class="container-educação">
+        <div class="row linha1">
+            <div class="col-md-6 escola">
+                <label for="escola"><strong>Escola:</strong></label>
+                <input type="text" name="escola" id="escola" class="form-control" tabindex="1">
+            </div>
+            <div class="col-md-6 grau">
+                <label for="grau"><strong>Grau:</strong></label>
+                <input class="form-control" type="text" name="grau">
+            </div>
+        </div>
+        <div class="row linha2">
+            <div class="col-md-6 data1">
+                <label for="data"><strong>Data de inicio:</strong></label>
+                <input type="date" name="data" id="data" class="form-control" tabindex="1">
+            </div>
+            <div class="col-md-6 data2">
+                <label for="data"><strong>Data de termino:</strong></label>
+                <input type="date" name="data2" id="data2" class="form-control" tabindex="1">
+            </div>
+        </div>
+        <div class="row linha3">
+            <div class="col-md-6 estado2">
+                <label for="estado"><strong>Estado:</strong></label>
+                <input type="text" name="estado" id="estado" class="form-control" tabindex="1">
+            </div>
+            <div class="col-md-6 cidade2">
+                <label for="empregador"><strong>Cidade:</strong></label>
+                <input type="text" name="cidade" id="cidade" class="form-control" tabindex="1">
+            </div>
+        </div>
+    </div>
+    <div id="dvCentro">
+        <textarea id="txtArtigo" name="txtArtigo"></textarea>
+    </div>
+    script src="ckeditor/ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace('txtArtigo');
+    </script>
     `; 
 }
