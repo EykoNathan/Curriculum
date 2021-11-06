a9741c6a5eebbda4b89111237f0660d93f0ee5de
EykoNathan <90914758+EykoNathan@users.noreply.github.com>
2021-11-06 08:07:12 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+var employment = 1;
 function Mudarestado() {
         var display = document.getElementById("minhaDiv").style.display;
         if(display == "block"){
@@ -22,41 +23,37 @@ function conteudoDivEmpregos() {
         <div class="row linha1">
             <div class="col-md-6 cargo2">
                 <label for="cargo"><strong>Cargo:</strong></label>
-                <input type="text" name="cargo" id="cargo2" class="form-control" tabindex="1">
+                <input type="text" name="cargo" id="cargo_${employment}" class="form-control" tabindex="1">
             </div>
             <div class="col-md-6 emprego2">
                 <label for="empregador"><strong>Empregador:</strong></label>
-                <input class="form-control" type="text" name="empregador">
+                <input class="form-control" id="empregador_${employment}"" type="text" name="empregador">
             </div>
         </div>
         <div class="row linha2">
             <div class="col-md-6 data1">
                 <label for="data"><strong>Data de inicio:</strong></label>
-                <input type="date" name="data" id="data" class="form-control" tabindex="1">
+                <input type="date" name="data" id="data_inicio_${employment}"" class="form-control" tabindex="1">
             </div>
             <div class="col-md-6 data2">
                 <label for="data"><strong>Data de termino:</strong></label>
-                <input type="date" name="data2" id="data2" class="form-control" tabindex="1">
+                <input type="date" name="data2" id="data_termino_${employment}" class="form-control" tabindex="1">
             </div>
         </div>
         <div class="row linha3">
             <div class="col-md-6 estado2">
                 <label for="estado"><strong>Estado:</strong></label>
-                <input type="text" name="estado" id="estado" class="form-control" tabindex="1">
+                <input type="text" name="estado" id="estado_${employment}" class="form-control" tabindex="1">
             </div>
             <div class="col-md-6 cidade2">
                 <label for="empregador"><strong>Cidade:</strong></label>
-                <input type="text" name="cidade" id="cidade" class="form-control" tabindex="1">
+                <input type="text" name="cidade" id="cidade_${employment}" class="form-control" tabindex="1">
             </div>
         </div>
+        <div id="dvCentro">
+            <textarea id="txtArtigo" name="txtArtigo"></textarea>
+        </div>
     </div>
-    <div id="dvCentro">
-        <textarea id="txtArtigo" name="txtArtigo"></textarea>
-    </div>
-    script src="ckeditor/ckeditor/ckeditor.js"></script>
-    <script>
-        CKEDITOR.replace('txtArtigo');
-    </script>
     `; 
 }
 function mostraDivLinks() {
@@ -70,11 +67,11 @@ function conteudoDivLinks() {
         <div class="row linha1">
             <div class="col-md-6 rotulo">
                 <label for="rotulo"><strong>Rotulo:</strong></label>
-                <input type="text" name="rotulo" id="rotulo" class="form-control" tabindex="1">
+                <input type="text" name="rotulo" id="rotulo_${employment}" class="form-control" tabindex="1">
             </div>
             <div class="col-md-6 link">
                 <label for="link"><strong>Link:</strong></label>
-                <input class="form-control" type="text" name="link">
+                <input class="form-control" id="link_${employment}" type="text" name="link">
             </div>
         </div>
     </div>
@@ -91,40 +88,40 @@ function conteudoDivEducação() {
         <div class="row linha1">
             <div class="col-md-6 escola">
                 <label for="escola"><strong>Escola:</strong></label>
-                <input type="text" name="escola" id="escola" class="form-control" tabindex="1">
+                <input type="text" name="escola" id="escola_${employment}" class="form-control" tabindex="1">
             </div>
             <div class="col-md-6 grau">
                 <label for="grau"><strong>Grau:</strong></label>
-                <input class="form-control" type="text" name="grau">
+                <input class="form-control" id="grau_${employment}" type="text" name="grau">
             </div>
         </div>
         <div class="row linha2">
             <div class="col-md-6 data1">
                 <label for="data"><strong>Data de inicio:</strong></label>
-                <input type="date" name="data" id="data" class="form-control" tabindex="1">
+                <input type="date" name="data" id="inicio_data_${employment}" class="form-control" tabindex="1">
             </div>
             <div class="col-md-6 data2">
                 <label for="data"><strong>Data de termino:</strong></label>
-                <input type="date" name="data2" id="data2" class="form-control" tabindex="1">
+                <input type="date" name="data2" id="termino_data_${employment}" class="form-control" tabindex="1">
             </div>
         </div>
         <div class="row linha3">
             <div class="col-md-6 estado2">
                 <label for="estado"><strong>Estado:</strong></label>
-                <input type="text" name="estado" id="estado" class="form-control" tabindex="1">
+                <input type="text" name="estado" id="estado_${employment}" class="form-control" tabindex="1">
             </div>
             <div class="col-md-6 cidade2">
                 <label for="empregador"><strong>Cidade:</strong></label>
-                <input type="text" name="cidade" id="cidade" class="form-control" tabindex="1">
+                <input type="text" name="cidade" id="cidade_${employment}" class="form-control" tabindex="1">
             </div>
         </div>
+        <div id="dvCentro">
+            <textarea id="txtArtigo" name="txtArtigo"></textarea>
+        </div>
+        <script src="ckeditor/ckeditor/ckeditor.js"></script>
+        <script>
+            CKEDITOR.replace('txtArtigo');
+        </script>
     </div>
-    <div id="dvCentro">
-        <textarea id="txtArtigo" name="txtArtigo"></textarea>
-    </div>
-    script src="ckeditor/ckeditor/ckeditor.js"></script>
-    <script>
-        CKEDITOR.replace('txtArtigo');
-    </script>
     `; 
 }
